325d594f999c19a11534c02596b6b0a6aac13aa9
ChrisVieira <sarge3do99@gmail.com>
2016-03-02 11:41:09 -0500

    Added gyro + made the program functional

--- src/org/usfirst/frc/team5112/robot/Robot.java
+++ src/org/usfirst/frc/team5112/robot/Robot.java
@@ -2,19 +2,20 @@ package org.usfirst.frc.team5112.robot;
 
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Talon;
-import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.RobotDrive.MotorType;
+import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Robot extends SampleRobot {
+	private double initialGyro = 0;
 	RobotDrive myRobot;
 	Joystick stick;
 	Talon FL;
 	Talon FR;
 	Talon BR;
 	Talon BL;
+	AnalogGyro gyro;
 //	final String defaultAuto = "Default";
 //	final String customAuto = "My Auto";
 //	SendableChooser chooser;
@@ -26,7 +27,8 @@ public class Robot extends SampleRobot {
 		FL = new Talon(2);
 		BR = new Talon(0);
 		BL = new Talon(1);
-		
+		gyro = new AnalogGyro(0);
+	
 		myRobot = new RobotDrive(BL, BR, FL, FR);
 		myRobot.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, false);
 		myRobot.setInvertedMotor(RobotDrive.MotorType.kRearLeft, false);
@@ -43,9 +45,19 @@ public class Robot extends SampleRobot {
 	public void operatorControl() {
 		myRobot.setSafetyEnabled(true);
 		while (isOperatorControl() && isEnabled()) {			
-			myRobot.arcadeDrive(stick); // drive with arcade style (use right
-										// stick)
-			Timer.delay(0.005); // wait for a motor update time
+			double y = stick.getY();
+			y = Math.max(-1, y);
+			y = Math.min(1, y);
+			double rotation = stick.getZ();
+			if (Math.abs(rotation) >= 0.1) {
+				myRobot.arcadeDrive(3 * Math.copySign(1, y) * Math.pow(y, 2) / 4.0,
+						3.0 * Math.copySign(1, stick.getZ()) * Math.pow(stick.getZ(), 2) / 4.0);
+				initialGyro = gyro.getAngle();
+			} else {
+				myRobot.arcadeDrive(3 * Math.copySign(1, y) * Math.pow(y, 2) / 4.0,
+						-0.03 * (gyro.getAngle() - initialGyro));
+			}
+			Timer.delay(0.005);
 		}
 	}
 
